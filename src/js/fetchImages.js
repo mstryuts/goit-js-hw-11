a71d63a0a0969c5ac695f3bcd15f8ae9db83bc99
maksim stryuts <maxim.stryuts@gmail.com>
2022-08-27 18:11:04 +0300

    .

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,32 +1,24 @@
 import axios from "axios";
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '29488809-58b059500460d672a67371786';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '29488809-58b059500460d672a67371786';
 
-
-// export async function fetchImages(query, page) {
-//   try {
-//       return await axios.get(BASE_URL, {
-//           params: {
-//               key: API_KEY,
-//               query,
-//               image_type: 'photo',
-//               orientation: 'horizontal',
-//               safesearch: true,
-//               per_page: 40,
-//               page,
-//         }
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-export async function fetchImages(query, page) {
+export async function fetchImages(q, page) {
   try {
-    return await axios.get(`https://pixabay.com/api/?key=29488809-58b059500460d672a67371786&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-
+      return await axios
+      .get(BASE_URL, {
+        params: {
+          key: API_KEY,
+          q,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+          page,
+        }
+    });
   } catch (error) {
     console.error(error);
   }
 }
+
